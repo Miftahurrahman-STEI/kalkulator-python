9020d7060914aba55e007423c65848289bcffdde
Miftahurrahman-STEI <mippganz@gmail.com>
2025-09-16 12:01:09 +0700

    first commit

---
+++ kalkulator.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+
+def tekan_tombol(nilai):
+    teks_sekarang = layar_var.get()
+    layar_var.set(teks_sekarang + str(nilai))
+
+def hitung_hasil():
+    try:
+        hasil = str(eval(layar_var.get()))
+        layar_var.set(hasil)
+    except:
+        layar_var.set("Error")
+
+def hapus_layar():
+    layar_var.set("")
+
+
+# Buat jendela utama
+window = tk.Tk()
+window.title("Kalkulator")
+window.geometry("400x450")
+window.resizable(False, False)
+
+# Buat layar input
+layar_var = tk.StringVar()
+layar = tk.Entry(window, textvariable=layar_var, font=("Arial", 20), justify="right", bd=10)
+layar.pack(fill="both", ipadx=8, ipady=10, pady=10)
+
+# Frame untuk tombol
+frame_tombol = tk.Frame(window)
+frame_tombol.pack()
+
+# Susunan tombol kalkulator
+tombol_list = [
+    ('7', '8', '9', '/'),
+    ('4', '5', '6', '*'),
+    ('1', '2', '3', '-'),
+    ('0', 'C', '=', '+')
+]
+
+# Buat tombol satu persatu
+for baris, row in enumerate(tombol_list):
+    for kolom, teks in enumerate(row):
+        if teks == "=":
+            tombol = tk.Button(frame_tombol, text=teks, width=5, height=2, command=hitung_hasil, font=('Arial', 18))
+        elif teks == "C":
+            tombol = tk.Button(frame_tombol, text=teks, width=5, height=2, command=hapus_layar, font=('Arial', 18))
+        else:
+            tombol = tk.Button(frame_tombol, text=teks, width=5, height=2, command=lambda t=teks: tekan_tombol(t), font=('Arial', 18))
+        tombol.grid(row=baris, column=kolom, padx=5, pady=5)
+
+window.mainloop()
---
+++ soal.py
@@ -0,0 +1,75 @@
+"""
+SOAL 1 MATERI VARIABEL & TIPE DATA :
+
+- Buat Variabel nama
+- Lalu Masukkan Input Nilai User di dalam Variabel nama
+
+- Buat Variabel umur
+- Lalu Masukkan Input Nilai User di dalam Variabel umur
+
+- Buat program yang meminta user memasukkan:
+    - nama
+    - umur
+
+Lalu cetak kalimat:
+
+Halo <nama>, 5 tahun lagi umurmu akan <umur + 5>
+
+ print ("pepe marti")
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+SOAL 2 MATERI VARIABEL & TIPE DATA :
+
+- Buat Variabel a = 10 dan b = 3.
+- Buat Variabel hasil untuk menghitung Variabel a dan b
+
+- Lalu Hitung menggunakan Operator Aritmatika :
+    - hitung menggunakan penjumlahan (+)
+    - hitung menggunakan pengurangan (-)
+    - hitung menggunakan perkalian (*)
+    - hitung menggunakan pembagian (/)
+
+Cetak hasilnya satu per satu.
+
+Output : 
+    13   (Penjumlahan)
+    11   (Pengurangan)
+    30   (Perkalian)
+    3.33 (Pembagian)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+SOAL 3 MATERI VARIABEL, TIPE DATA, PENGKONDISIAN :
+
+Buat variabel nilai = 75.
+Buat program yang menampilkan True jika nilai lebih besar atau sama dengan 70, dan False jika tidak.
+
+"""
+
